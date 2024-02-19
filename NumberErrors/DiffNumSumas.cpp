f62dcbda023b0d28b1f535d5d2b4c7deacbe5a8c
svargasro <svargasro@unal.edu.co>
2024-02-19 00:06:51 -0500

    DiffNumSumasArreglos

--- NumberErrors/DiffNumSumas.cpp
+++ NumberErrors/DiffNumSumas.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
 using std::cout;
-using std::cin;
 using std::endl;
+using std::vector;
 
 
-float s1(int N){
+
+vector<float> s1(int N){
+
+  vector<float> s1Vector(N);
+
   float s1 = 0;
-
   for (int k=1; k<=N ;k++) {
 
     s1+= 1.0/k;
+    s1Vector[k-1]=s1;
+
   }
-          return s1;
+          return s1Vector;
 }
 
-  float s2(int N){
-      float s2 = 0;
+vector<float> s2(int N){
 
-      for (int k=N; k>0 ;k--) {
-        s2+= 1.0/k;
+    vector<float> s2Vector(N);
+    float s2 = 0;
 
+    for (int k=N; k>0 ;k--) {
+      s2+= 1.0/k;
+      s2Vector[k-1]=s2;
       }
 
-      return s2;
+      return s2Vector;
     }
 
 
@@ -31,18 +39,18 @@ int main(int argc, char *argv[]) {
 
   int N = std::stoi(argv[1]);
 
-  float sum1;
-  float sum2;
-  float relativDif;
+  vector<float> sum1Vec = s1(N);
+  vector<float> sum2Vec = s2(N);
+  float relativDif, sum1, sum2;
 
   for (int i=1; i<=N ; i++ ) {
-    sum1=s1(i);
-    sum2=s2(i);
+    sum1= sum1Vec[i-1];
+    sum2= sum2Vec[i-1];
     relativDif = std::abs(1.0 - sum1/sum2);
     cout<<i<<" "<<sum1<<" "<<sum2<<" "<<" "<<relativDif<<" "<<endl;
   }
 
 
 
-                                                         return 0;
+  return 0;
 }
