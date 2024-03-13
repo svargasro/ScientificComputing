3e0ac2e95f3c20ed7b7fc1793f956dc7546ac5d4
svargasro <svargasro@unal.edu.co>
2024-03-13 07:49:49 -0500

    Testing2

--- Modularization_Testing/test_primes.cpp
+++ Modularization_Testing/test_primes.cpp
@@ -1,6 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include "primes.h"
-#define CATCH_CONFIG_MAIN
+
 
 TEST_CASE( "Primes are computed", "[isPrime]" ) {
     REQUIRE( isPrime( 1) == false );
@@ -8,4 +8,4 @@ TEST_CASE( "Primes are computed", "[isPrime]" ) {
 
 
 
-//Compilar con g++ -std=c++17 printprimes.cpp primes.cpp -lCatch2 -lCatch2Main
+//Compilar con g++ -std=c++17 test_primes.cpp primes.cpp -lCatch2 -lCatch2Main
