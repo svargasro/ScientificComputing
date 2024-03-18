a4bd26653dc2e20d54b82f5e3eaec8958b99abd0
svargasro <svargasro@unal.edu.co>
2024-03-18 17:04:01 -0500

    codigoFuenteDebugging

--- Debugging/codigoFuente.cpp
+++ Debugging/codigoFuente.cpp
@@ -8,7 +8,7 @@ double baz(double x);
 
 int main (int argc, char **argv)
 {
-int ii, jj;
+  int ii, jj;
   ii =  0;
   jj = -1;
 
@@ -23,27 +23,27 @@ int ii, jj;
 
 int foo(int a, int b){
 
-if(a==0 || b==0 || bar(a,b)==0){
- std::cout<<"Los argumentos de la función foo deben ser distintos de 0 y si el primer argumento es positivo, 2 multiplicado por el primer argumento no puede ser igual al segundo argumento."<<std::endl;
- return EXIT_FAILURE;
- }
+  if(a==0 || b==0 || bar(a,b)==0){
+    std::cout<<"Los argumentos de la función foo deben ser distintos de 0 y si el primer argumento es positivo, 2 multiplicado por el primer argumento no puede ser igual al segundo argumento."<<std::endl;
+    return EXIT_FAILURE;
+  }
 
-return a/b + b/bar(a, b) + b/a;
+  return a/b + b/bar(a, b) + b/a;
 }
 
-   int bar(int a, int b)
+int bar(int a, int b)
 {
- int c = std::abs(a);
-return c + a - b;
+  int c = std::abs(a);
+  return c + a - b;
 }
 
 double baz(double x)
 {
-if (x > 0){
-  std::cout<<"El valor introducido en la función baz debe ser menor o igual a 0."<<std::endl;
-  return EXIT_FAILURE;}
-double v = 1-(x+1);
-return std::sqrt(v);
+  if (x > 0){
+    std::cout<<"El valor introducido en la función baz debe ser menor o igual a 0."<<std::endl;
+    return EXIT_FAILURE;}
+  double v = 1-(x+1);
+  return std::sqrt(v);
 }
 
 
