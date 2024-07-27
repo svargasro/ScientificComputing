31fd3bb6c4ed0305fdb57f15634bca331621b231
svargasro <svargasro@unal.edu.co>
2024-07-27 17:52:45 -0500

    slurm2

--- slurm/job.sh
+++ slurm/job.sh
@@ -1,1 +1,21 @@
-srun -p 12threads -n 18 stress -t 10 -c 1
+#SBATCH --partition=12threads
+#SBATCH --ntasks-per-node=12
+stress -t 10 -c 1
+
+
+
+
+#Este comando es equivalente a usar SBATCH
+#srun -p 12threads -n 18 stress -t 10 -c 1
+
+
+
+
+
+# # SBATCH --nodes=2
+# # BATCH --job-name="testmulti"
+# # SBATCH --account="HerrComp" # not used
+# # SBATCH --mail-type=ALL
+# # SBATCH --mail-user=wfoquendop@unal.edu.co
+# # SBATCH --time=01:00:00
+# # SBATCH --cpus-per-task=1
