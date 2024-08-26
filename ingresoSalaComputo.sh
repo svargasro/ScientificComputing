c92f13b388590a0b1611605a87cba143da6b691f
svargasro <svargasro@unal.edu.co>
2024-08-25 23:48:46 -0500

    ingresosSalas

--- ingresoSalaComputo.sh
+++ ingresoSalaComputo.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-cat sshSala.txt |xclip -selection clipboard
+echo -n "ssh -p 443 svargasro@168.176.35.111" | xclip -selection clipboard
+
+#!/bin/bash
+echo -n "ssh svargasro@168.176.8.235"  | xclip -selection clipboard
